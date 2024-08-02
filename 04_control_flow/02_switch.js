cba184490d33af9b0598b6fe1b65d295119626b1
Sohamcool411 <85698406+Sohamcool411@users.noreply.github.com>
2024-08-02 19:44:18 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,26 @@
+const month=1
+switch (month) {
+    case 1:
+    case 'Jan':
+        console.log('Jan');
+        break;
+
+    case 2:
+    case 'March':
+        console.log('Feb');
+        break;
+
+    case 3:
+    case 'March':
+        console.log('March');
+        break;
+        
+    case 4 :
+    case 'April':
+        console.log('April');
+        break;
+
+    default:
+        console.log('may-dec');
+        break;
+}
