59e9c0aba4d5a3cca6be622b4a3f5c4229842a5e
Sohamcool411 <85698406+Sohamcool411@users.noreply.github.com>
2024-08-02 20:43:00 +0000

    basic loops

---
+++ 05_iterators/01_.js
@@ -0,0 +1,18 @@
+//for
+for (let i = 0; i < 10; i+=2) {
+    const element = i;
+    //console.log(element);
+}
+
+//while
+let i=0
+while (i<10) {
+    //console.log(i);
+    i++
+}
+
+//do while
+do {
+    console.log(i);
+    i-=2
+} while (i>0);
