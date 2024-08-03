fb08a23638cff3fa6fe35b307109b7661bdb78bf
Sohamcool411 <85698406+Sohamcool411@users.noreply.github.com>
2024-08-03 15:38:09 +0000

    map ,reduce

---
+++ 05_iterators/05_reduce.js
@@ -0,0 +1,7 @@
+const myNums=[1,2,3,4]
+const initial_value=0
+
+const newNums=myNums.reduce((acc,cv) => acc+cv,initial_value)
+console.log(newNums);
+
+//reduce is basically use to do the sum of all the elements in object.
